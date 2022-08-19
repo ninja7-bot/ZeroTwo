b265ad64b6b2cf8f32cbf549e6be1ec87b60f709
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2022-08-19 12:48:10 +0530

    Update cust_filters_sql.py

--- tg_bot/modules/sql/cust_filters_sql.py
+++ tg_bot/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from ZeroTwo.modules.helper_funcs.msg_types import Types
-from ZeroTwo.modules.sql import BASE, SESSION
+from tg_bot.modules.helper_funcs.msg_types import Types
+from tg_bot.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):

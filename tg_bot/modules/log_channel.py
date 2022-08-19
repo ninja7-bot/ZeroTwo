05dbe3bfac9600638f24aa2598bee7341628ef55
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2022-08-19 12:41:24 +0530

    Update log_channel.py

--- tg_bot/modules/log_channel.py
+++ tg_bot/modules/log_channel.py
@@ -201,7 +201,7 @@ if is_module_loaded(FILENAME):
 ➩ forwarding the /setlog to the group
 """
 
-    __mod_name__ = "【ʟᴏɢ ᴄʜᴀɴɴᴇʟ】"
+    __mod_name__ = "Log Channel"
 
     LOG_HANDLER = CommandHandler("logchannel", logging, run_async=True)
     SET_LOG_HANDLER = CommandHandler("setlog", setlog, run_async=True)

8c7ace015cebd30eb7739360263eabd35b2813ae
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2022-08-23 12:24:34 +0530

    Update anti_channel.py

--- ZeroTwo/modules/anti_channel.py
+++ ZeroTwo/modules/anti_channel.py
@@ -1,11 +1,10 @@
 from telegram.ext.filters import Filters
-from ZeroTwo.modules.helper_funcs.decorators import botcmd, botmsg
+from tg_bot.modules.helper_funcs.decorators import botcmd, botmsg
 from telegram import Update, message
 from telegram.ext import CallbackContext
 from ..modules.helper_funcs.anonymous import user_admin, AdminPerms
 import html
-
-from ZeroTwo.ex_plugins.dbfunctions import antichannel_status, disable_antichannel, enable_antichannel
+from ..modules.sql.antichannel_sql import antichannel_status, disable_antichannel, enable_antichannel
 
 
 @botcmd(command="antichannel", group=100)
@@ -17,16 +16,16 @@ def set_antichannel(update: Update, context: CallbackContext):
     if len(args) > 0:
         s = args[0].lower()
         if s in ["yes", "on"]:
-            enable_antichannel(chat_id=chat.id)
+            enable_antichannel(chat.id)
             message.reply_html("Enabled antichannel in {}".format(html.escape(chat.title)))
         elif s in ["off", "no"]:
-            disable_antichannel(chat_id=chat.id)
+            disable_antichannel(chat.id)
             message.reply_html("Disabled antichannel in {}".format(html.escape(chat.title)))
         else:
             message.reply_text("Unrecognized arguments {}".format(s))
         return
     message.reply_html(
-        "Antichannel setting is currently {} in {}".format(antichannel_status(chat_id=chat.id), html.escape(chat.title)))
+        "Antichannel setting is currently {} in {}".format(antichannel_status(chat.id), html.escape(chat.title)))
 
 
 @botmsg(Filters.chat_type.groups, group=110)
@@ -34,12 +33,9 @@ def eliminate_channel(update: Update, context: CallbackContext):
     message = update.effective_message
     chat = update.effective_chat
     bot = context.bot
-    if not antichannel_status(chat_id=chat.id):
+    if not antichannel_status(chat.id):
         return
     if message.sender_chat and message.sender_chat.type == "channel" and not message.is_automatic_forward:
         message.delete()
         sender_chat = message.sender_chat
         bot.ban_chat_sender_chat(sender_chat_id=sender_chat.id, chat_id=chat.id)
-        
-
-

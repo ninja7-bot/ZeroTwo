ff5fdffa930590a1273e2a14b18ac953e144967a
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2022-08-23 21:36:29 +0530

    Update anti_nsfw.py

--- ZeroTwo/modules/anti_nsfw.py
+++ ZeroTwo/modules/anti_nsfw.py
@@ -219,7 +219,7 @@ async def nsfw_scan_command(_, message: Message):
     file_id = get_file_id(reply)
     if not file_id:
         return await m.edit("Something went wrong.")
-    file= await download_file(location=message, out="..NSFW/")       
+    file= await download_file(client=zbot,location=message, out="..NSFW/")       
     try:
         results = await arq.nsfw_scan(file=file)
     except Exception as e:

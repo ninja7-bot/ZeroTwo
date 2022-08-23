fe4db57c1a6c6a37a431300e15e6c5efe2748184
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2022-08-23 19:52:20 +0530

    Update anti_nsfw.py

--- ZeroTwo/modules/anti_nsfw.py
+++ ZeroTwo/modules/anti_nsfw.py
@@ -138,7 +138,7 @@ async def nsfw_watcher(_, message: Message):
     file_id = get_file_id(message)
     file_unique_id = get_file_unique_id(message)
     if file_id and file_unique_id:
-        file = await download_file(file_id)
+        file = await download_file(message, out)
         try:
             resp = await arq.nsfw_scan(file=file)
         except Exception:
@@ -218,7 +218,7 @@ async def nsfw_scan_command(_, message: Message):
     file_id = get_file_id(reply)
     if not file_id:
         return await m.edit("Something went wrong.")
-    file = await download_file(file_id)
+    file = await download_file(message, out)
     try:
         results = await arq.nsfw_scan(file=file)
     except Exception as e:

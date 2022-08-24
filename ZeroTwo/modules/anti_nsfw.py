672c63c9a1cf24e1eb739402a2e1c7b0ab2eb2e0
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2022-08-24 16:41:40 +0530

    Update anti_nsfw.py

--- ZeroTwo/modules/anti_nsfw.py
+++ ZeroTwo/modules/anti_nsfw.py
@@ -1,5 +1,6 @@
 import os
 from os import remove
+import time
 
 from pyrogram import filters
 from pyrogram.types import Message
@@ -9,15 +10,24 @@ from ZeroTwo.modules.helper_funcs.telethn.chatstatus import (
 
 from ZeroTwo.utils.permissions import adminsOnly
 
+from telegram import ParseMode, ChatPermissions
+from telegram.error import BadRequest
+from telegram.utils.helpers import mention_html
+from telegram.ext import CommandHandler
+
 from ZeroTwo import arq, ZeroTwoTelethonClient as zbot
 from ZeroTwo.ex_plugins.dbfunctions import (disable_nsfw, disable_spam, enable_nsfw,
                           enable_spam, is_nsfw_enabled,
-                          is_spam_enabled)
-from ZeroTwo.modules.helper_funcs.chat_status import user_not_admin
-from telegram.ext import CallbackContext
+                          is_spam_enabled, set_nsfw_strength, get_nsfw_setting)
+from ZeroTwo.modules.helper_funcs.chat_status import user_not_admin, user_admin
+from ZeroTwo.modules.helper_funcs.alternate import send_message, typing_action
+from ZeroTwo.modules.log_channel import loggable
+from ZeroTwo.modules.warns import warn
+from ZeroTwo.modules.helper_funcs.string_handling import extract_time
+from ZeroTwo.modules.connection import connected
+from ZeroTwo.modules.sql.approve_sql import is_approved
+from ZeroTwo import dispatcher, LOGGER
 
-from ZeroTwo.FastTelethon import download_file
-from telethon.utils import resolve_bot_file_id as res
 
 __mod_name__ = "Anti-NSFW"
 
@@ -39,10 +49,6 @@ def get_file_id(message):
 
     if message.sticker:
         if message.sticker.is_animated:
-            if not message.sticker.thumbs:
-                return
-            return message.sticker.thumbs[0].file_id
-        if message.sticker.is_video:
             if not message.sticker.thumbs:
                 return
             return message.sticker.thumbs[0].file_id
@@ -68,34 +74,127 @@ def get_file_unique_id(message):
         return
     return m.file_unique_id
 
-async def download(message):
-    media = message.media
-    if hasattr(media, "document"):
-        msg = media.document
-        mime_type = file.mime_type
-        filename = message.file.name
-        if not filename:
-            if "video" in mime_type:
-                filename = (
-                    "video_" +
-                    datetime.now().isoformat(
-                        "_",
-                        "seconds") +
-                    ".mp4")
-            elif "gif" in mime_type:
-                filename = (
-                    "gif_" +
-                    datetime.now().isoformat(
-                            "_",
-                            "seconds") +
-                        ".gif")
-        file = await download_file(client=zbot, location=msg, out="NSFW/")
-        return file
+
+@loggable
+@user_admin
+@typing_action
+def nsfw_mode(update, context):
+    chat = update.effective_chat
+    user = user
+    msg = update.effective_message
+    args = context.args
+
+    conn = connected(context.bot, update, chat, user.id, need_admin=True)
+    if conn:
+        chat = dispatcher.bot.getChat(conn)
+        chat_id = conn
+        chat_name = dispatcher.bot.getChat(conn).title
     else:
-        file_id=get_file_id(message)
-        file = await zbot.download_media(file_id)
-        return file
-    
+        if update.effective_message.chat.type == "private":
+            send_message(
+                update.effective_message,
+                "This command can be only used in group not in PM",
+            )
+            return ""
+        chat = update.effective_chat
+        chat_id = update.effective_chat.id
+        chat_name = update.effective_message.chat.title
+
+    if args:
+        if args[0].lower() in ["default"]:
+            settypensfw = "default"
+            set_nsfw_strength(chat_id, 0, "0")
+        elif args[0].lower() in ["ban"]:
+            settypensfw = "ban the sender"
+            set_nsfw_strength(chat_id, 1, "0")
+        elif args[0].lower() == "warn":
+            settypensfw = "warn the sender"
+            set_nsfw_strength(chat_id, 2, "0")
+        elif args[0].lower() == "mute":
+            settypensfw = "mute the sender"
+            set_nsfw_strength(chat_id, 3, "0")
+        elif args[0].lower() == "kick":
+            settypensfw = "kick the sender"
+            set_nsfw_strength(chat_id, 4, "0")
+        elif args[0].lower() == "tban":
+            if len(args) == 1:
+                teks = """It looks like you tried to set time value for nsfw but you didn't specified time; Try, `/nsfwmode tban <timevalue>`.
+    Examples of time value: 4m = 4 minutes, 3h = 3 hours, 6d = 6 days, 5w = 5 weeks."""
+                send_message(update.effective_message, teks, parse_mode="markdown")
+                return ""
+            restime = extract_time(msg, args[1])
+            if not restime:
+                teks = """Invalid time value!
+    Example of time value: 4m = 4 minutes, 3h = 3 hours, 6d = 6 days, 5w = 5 weeks."""
+                send_message(update.effective_message, teks, parse_mode="markdown")
+                return ""
+            settypensfw = "temporarily ban for {}".format(args[1])
+            set_nsfw_strength(chat_id, 5, str(args[1]))
+        elif args[0].lower() == "tmute":
+            if len(args) == 1:
+                teks = """It looks like you tried to set time value for nsfw but you didn't specified  time; try, `/nsfwmode tmute <timevalue>`.
+    Examples of time value: 4m = 4 minutes, 3h = 3 hours, 6d = 6 days, 5w = 5 weeks."""
+                send_message(update.effective_message, teks, parse_mode="markdown")
+                return ""
+            restime = extract_time(msg, args[1])
+            if not restime:
+                teks = """Invalid time value!
+    Examples of time value: 4m = 4 minutes, 3h = 3 hours, 6d = 6 days, 5w = 5 weeks."""
+                send_message(update.effective_message, teks, parse_mode="markdown")
+                return ""
+            settypensfw = "temporarily mute for {}".format(args[1])
+            set_nsfw_strength(chat_id, 6, str(args[1]))
+        else:
+            send_message(
+                update.effective_message,
+                "I only understand: default/warn/ban/kick/mute/tban/tmute!",
+            )
+            return ""
+        if conn:
+            text = "Changed nsfw mode: `{}` in *{}*!".format(
+                settypensfw,
+                chat_name,
+            )
+        else:
+            text = "Changed nsfw mode: `{}`!".format(settypensfw)
+        send_message(update.effective_message, text, parse_mode="markdown")
+        return (
+            "<b>{}:</b>\n"
+            "<b>Admin:</b> {}\n"
+            "Changed the nsfw mode. will {}.".format(
+                html.escape(chat.title),
+                mention_html(user.id, html.escape(user.first_name)),
+                settypensfw,
+            )
+        )
+    getmode, getvalue = get_nsfw_setting(chat.id)
+    if getmode == 0:
+        settypensfw = "default"
+    elif getmode == 1:
+        settypensfw = "ban"
+    elif getmode == 2:
+        settypensfw = "warn"
+    elif getmode == 3:
+        settypensfw = "mute"
+    elif getmode == 4:
+        settypensfw = "kick"
+    elif getmode == 5:
+        settypensfw = "temporarily ban for {}".format(getvalue)
+    elif getmode == 6:
+        settypensfw = "temporarily mute for {}".format(getvalue)
+    if conn:
+        text = "Current NSFW Mode: *{}* in *{}*.".format(
+            settypensfw,
+            chat_name,
+        )
+    else:
+        text = "Current NSFW Mode: *{}*.".format(settypensfw)
+    send_message(update.effective_message, text, parse_mode=ParseMode.MARKDOWN)
+    return ""
+BLACKLISTMODE_HANDLER = CommandHandler(
+    "nsfwmode", nsfw_mode, pass_args=True, run_async=True
+)
+
 @adminsOnly("can_change_info")
 @zbot.on_message(
     filters.command("antinsfw") & ~filters.private, group=3
@@ -150,6 +249,18 @@ async def spam_toggle_func(_, message: Message):
         )
 
 
+ALERT = """ 
+**NSFW Image Detected & Deleted Successfully!**
+**User:** {message.from_user.mention} [`{message.from_user.id}`]
+**Safe:** `{result.neutral} %`
+**Porn:** `{result.porn} %`
+**Adult:** `{result.sexy} %`
+**Hentai:** `{result.hentai} %`
+**Drawings:** `{result.drawings} %`
+
+Action has been taken against the user.
+"""
+
 
 @zbot.on_message(
     (
@@ -161,16 +272,24 @@ async def spam_toggle_func(_, message: Message):
         | filters.text
     )
 )
-async def nsfw_watcher(_, message: Message):
+@user_not_admin
+async def nsfw_watcher(context, message: Message):
     if not await is_nsfw_enabled(message.chat.id):
         return
     if not message.from_user:
         return
-
+    bot = context.bot
+    user = message.from_user
+    chat_id = message.chat.id
+    chat = message.bot.get_chat
     file_id = get_file_id(message)
     file_unique_id = get_file_unique_id(message)
+
+    if is_approved(chat_id, user.id):
+        return
+
     if file_id and file_unique_id:
-        file = await zbot.download_media(file_id)  
+        file = await zbot.download_media(file_id)
         try:
             resp = await arq.nsfw_scan(file=file)
         except Exception:
@@ -183,27 +302,93 @@ async def nsfw_watcher(_, message: Message):
         nsfw = result.is_nsfw
         if not nsfw:
             return
+        getmode, value = get_nsfw_setting(chat_id)
         try:
-            if can_delete_messages:
-                await message.delete()
-            else:
-                await message.reply_text("Delete Permissions not granted.")
-        except Exception:
-            return
-        await message.reply_text(
-            f""" 
-**NSFW Image Detected & Deleted Successfully!**
-**User:** {message.from_user.mention} [`{message.from_user.id}`]
-**Safe:** `{result.neutral} %`
-**Porn:** `{result.porn} %`
-**Adult:** `{result.sexy} %`
-**Hentai:** `{result.hentai} %`
-**Drawings:** `{result.drawings} %`
-
-Avoid sending NSFW messages.
-""")
-    
-
+            if getmode == 0:
+                bot.restrict_chat_member(
+                    chat.id,
+                    user.id,
+                    permissions=ChatPermissions(
+                        can_send_messages=True,
+                        can_send_media_messages=False,
+                        can_send_other_messages=False,
+                        can_invite_users=False,
+                        can_pin_messages=False,
+                        can_send_polls=False,
+                        can_change_info=False,
+                        can_add_web_page_previews=False,
+                    ),
+                    until_date=(int(time.time() + 24 * 60 * 60))),
+            elif getmode == 1:
+                message.delete()
+                chat.kick_member(user.id)
+                bot.sendMessage(
+                    chat.id,
+                    f"Banned {user.first_name} for sending NSFW Media.",
+                )
+                return
+            elif getmode == 2:
+                try:
+                    message.delete()
+                except BadRequest:
+                    pass
+                warn(
+                    user,
+                    chat,
+                    ("Using blacklisted trigger: {}"),
+                    message,
+                    user,
+                )
+                return
+            elif getmode == 3:
+                message.delete()
+                bot.restrict_chat_member(
+                    chat.id,
+                    user.id,
+                    permissions=ChatPermissions(can_send_messages=False),
+                )
+                bot.sendMessage(
+                    chat.id,
+                    f"Muted {user.first_name} for sending NSFW Media.!",
+                )
+                return
+            elif getmode == 4:
+                message.delete()
+                res = chat.unban_member(user.id)
+                if res:
+                    bot.sendMessage(
+                    chat.id,
+                        f"Kicked {user.first_name} for sending NSFW Media.!",
+                    )
+                return
+            elif getmode == 5:
+                message.delete()
+                bantime = extract_time(message, value)
+                chat.kick_member(user.id, until_date=bantime)
+                bot.sendMessage(
+                    chat.id,
+                    f"Banned {user.first_name} until '{value}' for sending NSFW Media.!",
+                )
+                return
+            elif getmode == 6:
+                message.delete()
+                mutetime = extract_time(message, value)
+                bot.restrict_chat_member(
+                    chat.id,
+                    user.id,
+                    until_date=mutetime,
+                    permissions=ChatPermissions(can_send_messages=False),
+                )
+                bot.sendMessage(
+                    chat.id,
+                    f"Muted {user.first_name} until '{value}' for sending NSFW Media.!",
+                )
+                return
+        except:
+            await message.reply_text("Delete Permissions not granted.")
+        
+        
+@user_not_admin
 async def spam_detect(_, message):
     if not is_spam_enabled(message.chat.id):
         return
@@ -250,7 +435,7 @@ async def nsfw_scan_command(_, message: Message):
     file_id = get_file_id(reply)
     if not file_id:
         return await m.edit("Something went wrong.")
-    file = await download(reply)
+    file = await zbot.download_media(file_id)
     try:
         results = await arq.nsfw_scan(file=file)
     except Exception as e:
@@ -289,4 +474,3 @@ async def scanNLP(_, message: Message):
 **Profanity:** {data.profanity}
 """
     await message.reply(msg, quote=True)
-    

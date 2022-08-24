4a2dca7057ad8e80afe5d41c9a6ea275f7743418
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2022-08-24 16:42:40 +0530

    Update dbfunctions.py

--- ZeroTwo/ex_plugins/dbfunctions.py
+++ ZeroTwo/ex_plugins/dbfunctions.py
@@ -299,6 +299,20 @@ async def disable_nsfw(chat_id: int):
         return
     return nsfwdb.insert_one({"chat_id": chat_id})
 
+
+#Hopefully works!
+async def set_nsfw_strength(chat_id: int, nsfwtype: int, value):
+    chat = nsfwdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return 
+    return nsfwdb.insert_one({"chat_id"}: chat_id, {"nsfwtype"}: nsfwtype, {"value"}: value)
+
+async def get_nsfw_strength(chat_id: int):
+    chat = nsfwdb.find_one({"chat_id"}: chat_id)
+    if not chat:
+        return
+    return chat["nsfw_type"], chat["value"]
+
 #SPAM Scanning
 async def is_spam_enabled(chat_id: int) -> bool:
     chat = spamdb.find_one({"chat_id": chat_id})

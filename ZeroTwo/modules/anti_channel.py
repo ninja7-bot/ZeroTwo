e89a443c3d369ce76d195c1fb97ad4318939b487
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2022-08-25 12:42:25 +0530

    Update anti_channel.py

--- ZeroTwo/modules/anti_channel.py
+++ ZeroTwo/modules/anti_channel.py
@@ -1,43 +1,45 @@
-from telegram.ext.filters import Filters
-from ZeroTwo.modules.helper_funcs.decorators import botcmd, botmsg
-from telegram import Update, message
-from telegram.ext import CallbackContext, run_async
+from ZeroTwo.modules.helper_funcs.chat_status import user_not_admin
+
+from ZeroTwo.modules.helper_funcs.decorators import botcmd
+from telegram import Update
+from telegram.ext import CallbackContext
 from ..modules.helper_funcs.anonymous import user_admin, AdminPerms
 import html
-from ZeroTwo.ex_plugins.dbfunctions import antichannel_status, disable_antichannel, enable_antichannel
-from ZeroTwo import ZeroTwoTelethonClient as zbot
+from ..modules.sql.antichannel_sql import antichannel_status, disable_antichannel, enable_antichannel
+
 
 @botcmd(command="antichannel", group=100)
 @user_admin(AdminPerms.CAN_RESTRICT_MEMBERS)
 def set_antichannel(update: Update, context: CallbackContext):
     message = update.effective_message
     chat = update.effective_chat
-    chat_id = chat.id
     args = context.args
     if len(args) > 0:
         s = args[0].lower()
         if s in ["yes", "on"]:
-            enable_antichannel(chat_id)
+            enable_antichannel(chat.id)
             message.reply_html("Enabled antichannel in {}".format(html.escape(chat.title)))
         elif s in ["off", "no"]:
-            disable_antichannel(chat_id)
+            disable_antichannel(chat.id)
             message.reply_html("Disabled antichannel in {}".format(html.escape(chat.title)))
         else:
             message.reply_text("Unrecognized arguments {}".format(s))
         return
     message.reply_html(
-        "Antichannel setting is currently {} in {}".format(antichannel_status(chat_id), html.escape(chat.title)))
+        "Antichannel setting is currently {} in {}".format(antichannel_status(chat.id), html.escape(chat.title)))
 
-@run_async
-@botmsg(Filters.chat_type.groups, group=110)
-def eliminate_channel(update: Update, context: CallbackContext):
+@user_not_admin
+async def eliminate_channel(update: Update, context: CallbackContext):
     message = update.effective_message
     chat = update.effective_chat
-    chat_id = chat.id
     bot = context.bot
-    if not antichannel_status(chat_id):
-        return
-    if message.sender_chat and message.sender_chat.type == "channel" and not message.is_automatic_forward:
-        message.delete()
-        sender_chat = message.sender_chat
-        bot.ban_chat_sender_chat(sender_chat_id=sender_chat.id, chat_id=chat.id)
+    if chat.type == chat.GROUP or chat.SUPERGROUP:
+        if await antichannel_status(chat.id):
+            if message.send_chat and message.send_chat.type == "channel" and not message.is_automatic_forward:
+                message.delete()
+                sender_chat = message.sender_chat
+                bot.ban_chat_sender_chat(sender_chat_id=sender_chat.id, chat_id=chat.id)
+            else:
+                return
+        else:
+            return

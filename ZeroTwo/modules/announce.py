4fc0b159e38d28dd2623b140952773e61a1a1442
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2022-09-02 16:23:19 +0530

    Update announce.py

--- ZeroTwo/modules/announce.py
+++ ZeroTwo/modules/announce.py
@@ -4,11 +4,11 @@ from telegram import Update
 from telegram.ext import CallbackContext
 from telegram.utils.helpers import mention_html
 
-from ZeroTwo.ex_plugins.dbfunctions import does_chat_log, enable_chat_log, disable_chat_log
-from ZeroTwo.modules.helper_funcs.anonymous import AdminPerms
-from ZeroTwo.modules.helper_funcs.anonymous import user_admin as u_admin
-from ZeroTwo.modules.helper_funcs.decorators import botcmd
 from ZeroTwo.modules.log_channel import loggable
+from ZeroTwo.modules.helper_funcs.decorators import botcmd
+
+import ZeroTwo.modules.sql.logger_sql as sql
+from ..modules.helper_funcs.anonymous import user_admin as u_admin, AdminPerms
 
 
 @botcmd(command="announce", pass_args=True)
@@ -17,45 +17,43 @@ from ZeroTwo.modules.log_channel import loggable
 def announcestat(update: Update, context: CallbackContext) -> str:
     args = context.args
     if len(args) > 0:
-        update.effective_user
+        u = update.effective_user
         message = update.effective_message
         chat = update.effective_chat
         user = update.effective_user
-        chat_id=chat.id
         if args[0].lower() in ["on", "yes", "true"]:
-            enable_chat_log(chat_id)
+            sql.enable_chat_log(update.effective_chat.id)
             update.effective_message.reply_text(
                 "I've enabled announcemets in this group. Now any admin actions in your group will be announced."
             )
-            log_message = (
+            logmsg = (
                 f"<b>{html.escape(chat.title)}:</b>\n"
                 f"#ANNOUNCE_TOGGLED\n"
                 f"Admin actions announcement has been <b>enabled</b>\n"
                 f"<b>Admin:</b> {mention_html(user.id, user.first_name) if not message.sender_chat else message.sender_chat.title}\n "
             )
-            return log_message
+            return logmsg
         elif args[0].lower() in ["off", "no", "false"]:
-            disable_chat_log(chat_id)
+            sql.disable_chat_log(update.effective_chat.id)
             update.effective_message.reply_text(
                 "I've disabled announcemets in this group. Now admin actions in your group will not be announced."
             )
-            log_message = (
+            logmsg = (
                 f"<b>{html.escape(chat.title)}:</b>\n"
                 f"#ANNOUNCE_TOGGLED\n"
                 f"Admin actions announcement has been <b>disabled</b>\n"
                 f"<b>Admin:</b> {mention_html(user.id, user.first_name) if not message.sender_chat else message.sender_chat.title}\n "
             )
-            return log_message
+            return logmsg
     else:
-        if does_chat_log(chat_it):
-            result=True
         update.effective_message.reply_text(
             "Give me some arguments to choose a setting! on/off, yes/no!\n\n"
-            "Your current setting is: {result}\n"
+            "Your current setting is: {}\n"
             "When True, any admin actions in your group will be announced."
-            "When False, admin actions in your group will not be announced."
-            )
-        return ""
+            "When False, admin actions in your group will not be announced.".format(
+                sql.does_chat_log(update.effective_chat.id))
+        )
+        return ''
 
 
 def __migrate__(old_chat_id, new_chat_id):

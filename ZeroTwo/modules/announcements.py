b1aade48c4d703b7dfe53ddb8dea4749685cf5c9
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2022-09-02 16:24:28 +0530

    Update announcements.py

--- ZeroTwo/modules/announcements.py
+++ ZeroTwo/modules/announcements.py
@@ -1,19 +1,19 @@
 import html
 from typing import Optional
 
-from telegram import ParseMode, Update
-from telegram.chatmemberupdated import ChatMemberUpdated
+from telegram import Update, ParseMode
 from telegram.ext import CallbackContext
+from telegram.chatmemberupdated import ChatMemberUpdated
 from telegram.ext.chatmemberhandler import ChatMemberHandler
-
-from ZeroTwo.ex_plugins.dbfunctions import does_chat_log
-
+import ZeroTwo.modules.sql.log_channel_sql as logsql
 from ZeroTwo import dispatcher
 from ZeroTwo.modules.log_channel import loggable
 
+import ZeroTwo.modules.sql.logger_sql as sql
+
+
 # Module to extract and log (optional: send to chat) status changes in chat members using ChatMemberUpdated
 # https://github.com/python-telegram-bot/python-telegram-bot/blob/master/examples/chatmemberbot.py
-
 
 def extract_status_change(chat_member_update: ChatMemberUpdated):
     try:
@@ -30,7 +30,7 @@ def extract_status_change(chat_member_update: ChatMemberUpdated):
 
 
 def do_announce(chat):  # announce to chat or only to log channel?
-    return bool(chat.type != "channel" and does_chat_log(chat_id))
+    return bool(chat.type != "channel" and sql.does_chat_log(chat.id))
 
 
 @loggable
@@ -38,20 +38,22 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
     bot = context.bot
     chat = update.effective_chat
     message = update.effective_message
-
+    log_setting = logsql.get_chat_setting(chat.id)
+    if not log_setting:
+        logsql.set_chat_setting(logsql.LogChannelSettings(chat.id, True, True, True, True, True))
+        log_setting = logsql.get_chat_setting(chat.id)
+        
     result = extract_status_change(update.chat_member)
     status_change, title_change = result
 
-    if (
-        title_change is not None and status_change is None
-    ):  # extract title changes for admins
+    if title_change is not None and status_change is None:  # extract title changes for admins
         oldtitle, newtitle = title_change
         cause_name = update.chat_member.from_user.mention_html()
         member_name = update.chat_member.new_chat_member.user.mention_html()
         if oldtitle != newtitle:
 
             if str(update.chat_member.from_user.id) == str(bot.id):  # bot action
-                return ""  # we handle these in their respective modules
+                return ''  # we handle these in their respective modules
             else:
 
                 if oldtitle is None:
@@ -73,7 +75,8 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
                 elif newtitle is None:
                     if do_announce(chat):
                         update.effective_chat.send_message(
-                            f"{member_name}'s title was removed by {cause_name}.\nold title: '<code>{oldtitle}</code>'\nnew title: {newtitle}",
+                            f"{member_name}'s title was removed by {cause_name}.\nold title: '<code>{oldtitle}</code"
+                            f">'\nnew title: {newtitle}",
                             parse_mode=ParseMode.HTML,
                         )
                     log_message = (
@@ -89,7 +92,8 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
                 else:
                     if do_announce(chat):
                         update.effective_chat.send_message(
-                            f"{member_name}'s title was changed by {cause_name}.\nold title: '<code>{oldtitle}</code>'\nnew title: '<code>{newtitle}</code>'",
+                            f"{member_name}'s title was changed by {cause_name}.\nold title: '<code>{oldtitle}</code"
+                            f">'\nnew title: '<code>{newtitle}</code>'",
                             parse_mode=ParseMode.HTML,
                         )
                     log_message = (
@@ -103,12 +107,12 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
                     return log_message
 
     if status_change is not None:  # exctract chat changes
-        status = ",".join(status_change)
+        status = ','.join(status_change)
         oldstat = str(status.split(",")[0])
         newstat = str(status.split(",")[1])
 
         if str(update.chat_member.from_user.id) == str(bot.id):
-            return ""  # we handle these in their respective modules same as before
+            return ''  # we handle these in their respective modules same as before
         else:
 
             cause_name = update.chat_member.from_user.mention_html()
@@ -120,6 +124,10 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
                         f"{member_name} was demoted by {cause_name}.",
                         parse_mode=ParseMode.HTML,
                     )
+
+                if not log_setting.log_action:
+                    return ""
+
                 log_message = (
                     f"<b>{html.escape(chat.title)}:</b>\n"
                     f"#ADMIN\n<b>Demoted</b>\n"
@@ -131,9 +139,13 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
             if oldstat == "administrator" and newstat == "kicked":
                 if do_announce(chat):
                     update.effective_chat.send_message(
-                        f"{member_name} was demoted anad removed by {cause_name}.",
+                        f"{member_name} was demoted and removed by {cause_name}.",
                         parse_mode=ParseMode.HTML,
                     )
+
+                if not log_setting.log_action:
+                    return ""
+
                 log_message = (
                     f"<b>{html.escape(chat.title)}:</b>\n"
                     f"#BANNED\n"
@@ -144,6 +156,10 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
                 return log_message
 
             if oldstat == "administrator" and newstat == "left":
+
+                if not log_setting.log_action:
+                    return ""
+
                 log_message = (
                     f"<b>{html.escape(chat.title)}:</b>\n"
                     f"#ADMIN\n<b>Left</b>\n"
@@ -161,6 +177,10 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
                                 f"{member_name} was promoted by {cause_name} with the title <code>{newtitle}</code>.",
                                 parse_mode=ParseMode.HTML,
                             )
+
+                        if not log_setting.log_action:
+                            return ""
+
                         log_message = (
                             f"<b>{html.escape(chat.title)}:</b>\n"
                             f"#ADMIN\n<b>Promoted</b>\n"
@@ -176,6 +196,10 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
                             f"{member_name} was promoted by {cause_name}.",
                             parse_mode=ParseMode.HTML,
                         )
+
+                    if not log_setting.log_action:
+                        return ""
+
                     log_message = (
                         f"<b>{html.escape(chat.title)}:</b>\n"
                         f"#ADMIN\n<b>Promoted</b>\n"
@@ -190,6 +214,10 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
                         f"{member_name} was muted by {cause_name}.",
                         parse_mode=ParseMode.HTML,
                     )
+
+                if not log_setting.log_action:
+                    return ""
+
                 log_message = (
                     f"<b>{html.escape(chat.title)}:</b>\n"
                     f"#MUTED\n"
@@ -204,6 +232,10 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
                         f"{member_name} was unmuted by {cause_name}.",
                         parse_mode=ParseMode.HTML,
                     )
+
+                if not log_setting.log_action:
+                    return ""
+
                 log_message = (
                     f"<b>{html.escape(chat.title)}:</b>\n"
                     f"#UNMUTED\n"
@@ -223,6 +255,10 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
                     f"{member_name} was banned by {cause_name}.",
                     parse_mode=ParseMode.HTML,
                 )
+
+            if not log_setting.log_action:
+                return ""
+
             log_message = (
                 f"<b>{html.escape(chat.title)}:</b>\n"
                 f"#BANNED\n"
@@ -237,6 +273,10 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
                     f"{member_name} was unbanned by {cause_name}.",
                     parse_mode=ParseMode.HTML,
                 )
+
+            if not log_setting.log_action:
+                return ""
+
             log_message = (
                 f"<b>{html.escape(chat.title)}:</b>\n"
                 f"#UNBANNED\n"
@@ -251,6 +291,10 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
                     f"{member_name} was unbanned and added by {cause_name}.",
                     parse_mode=ParseMode.HTML,
                 )
+
+            if not log_setting.log_action:
+                return ""
+
             log_message = (
                 f"<b>{html.escape(chat.title)}:</b>\n"
                 f"#UNBANNED\n"
@@ -262,6 +306,10 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
 
         if oldstat == ("left" or "kicked") and newstat == "member":
             if member_name == cause_name:
+
+                if not log_setting.log_joins:
+                    return ""
+
                 log_message = (
                     f"<b>{html.escape(chat.title)}:</b>\n"
                     f"#WELCOME\n"
@@ -271,6 +319,9 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
                 return log_message
 
             else:
+                if not log_setting.log_joins:
+                    return ""
+
                 log_message = (
                     f"<b>{html.escape(chat.title)}:</b>\n"
                     f"#WELCOME\n"
@@ -282,6 +333,10 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
 
         if oldstat == ("member" or "administrator") and newstat == "left":
             if member_name == cause_name:
+
+                if not log_setting.log_leave:
+                    return ""
+
                 log_message = (
                     f"<b>{html.escape(chat.title)}:</b>\n"
                     f"#GOODBYE\n"
@@ -291,6 +346,10 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
                 return log_message
 
             else:
+
+                if not log_setting.log_leave:
+                    return ""
+
                 log_message = (
                     f"<b>{html.escape(chat.title)}:</b>\n"
                     f"#REMOVED\n"
@@ -301,6 +360,4 @@ def chatmemberupdates(update: Update, context: CallbackContext) -> Optional[str]
                 return log_message
 
 
-dispatcher.add_handler(
-    ChatMemberHandler(chatmemberupdates, ChatMemberHandler.CHAT_MEMBER, run_async=True)
-)
+dispatcher.add_handler(ChatMemberHandler(chatmemberupdates, ChatMemberHandler.CHAT_MEMBER, run_async=True))

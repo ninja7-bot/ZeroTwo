2f2623a3d8faf44ec6b8dbfb91914600b0affc8e
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2022-09-06 19:32:58 +0530

    Update anti_channel.py

--- ZeroTwo/modules/anti_channel.py
+++ ZeroTwo/modules/anti_channel.py
@@ -84,7 +84,7 @@ async def eliminate_user(_, message):
     return
   if first or last in network_names:
     try:
-      await zbot.ban_chat_member(chat_id, id=uid)
+      await zbot.ban_chat_member(chat_id, user_id=uid)
       await zbot.send_message(text=f"Banned `{uid}`: **{user.mention}** for Network Tag in name.", chat_id=message.chat.id)
     except:
       await zbot.send_message(text=f"Admin Rights are missing.\n{user.mention} have a network tag in their name.",chat_id=message.chat.id)
@@ -106,7 +106,7 @@ async def network_scan(_, message: Message):
   if user.last_name:
     last+=user.last_name
   if first or last in network_names:
-    await zbot.ban_chat_member(chat_id, id=uid)
+    await zbot.ban_chat_member(chat_id, user_id=uid)
     await zbot.send_message(text=f"**Network Tag** `{last}` found in name of {first}.", chat_id=message.chat.id)
   else:
     await zbot.send_message(text=f"{first} is not having any specified network tag in their name. Update network_names if they do have a network tag immediately.", chat_id=message.chat.id)
